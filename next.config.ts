9a8d1b48344fe31d33abc20af165ae8b46efa30a
Hamid <h55180d@gmail.com>
2025-09-04 16:36:12 +0300

    Deployments4

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,25 @@
 const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'flagsapi.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+typescript: {
+// !! WARN !!
+// Dangerously allow production builds to successfully complete even if
+// your project has type errors.
+// !! WARN !!
+ignoreBuildErrors: true,
+},
+eslint: {
+// Warning: This allows production builds to successfully complete even if
+// your project has ESLint errors.
+ignoreDuringBuilds: true,
+},
+images: {
+remotePatterns: [
+{
+protocol: 'https',
+hostname: 'flagsapi.com',
+port: '',
+pathname: '/**',
+},
+],
+},
 };
-
 export default nextConfig;
